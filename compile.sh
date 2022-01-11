1321a570b5195f91143fd44c38e827a7819f7fa0
Marko Bauhardt <mb@datameer.com>
2022-01-11 09:57:55 +0100

    use processor path to load annotation processors instead the classpath

--- compile.sh
+++ compile.sh
@@ -1,3 +1,11 @@
 GRADLE_REPO=~/.gradle/caches/modules-2/files-2.1
 
-java -jar jars/ecj-4.22.jar -failOnWarning -proceedOnError -time -progress -verbose -source 11 -target 11 -cp $GRADLE_REPO/com.google.auto.value/auto-value-annotations/1.9/25a0fcef915f663679fcdb447541c5d86a9be4ba/auto-value-annotations-1.9.jar:$GRADLE_REPO/com.google.auto.value/auto-value/1.9/fd4ec236e20ae0895c30e83260f611bcbc185086/auto-value-1.9.jar -d out/classes -s src/main/generated src/main/java/org/example/Coffee.java
+java\
+ -jar jars/ecj-4.22.jar\
+ -failOnWarning -proceedOnError -time -progress -verbose -g -deprecation -nowarn\
+ -source 11 -target 11\
+ -cp $GRADLE_REPO/com.google.auto.value/auto-value-annotations/1.9/25a0fcef915f663679fcdb447541c5d86a9be4ba/auto-value-annotations-1.9.jar\
+ -processorpath $GRADLE_REPO/com.google.auto.value/auto-value/1.9/fd4ec236e20ae0895c30e83260f611bcbc185086/auto-value-1.9.jar\
+ -d out/classes\
+ -s src/main/generated\
+ src/main/java/org/example/Coffee.java
